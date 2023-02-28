457fce07b7a5310dd182528731bdef2f34df5331
larisa <yeskovalarysa.cat@gmail.com>
2023-02-28 12:19:11 +0200

    add mui

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import { Button, Step, StepLabel, Stepper } from "@mui/material";
 import FifthQuestion from "./questions/QuestionFifth";
 import FirstQuestion from "./questions/QuestionFirst";
 import FourthQuestion from "./questions/QuestionFourth";
@@ -6,16 +7,27 @@ import SeventhQuestion from "./questions/QuestionSeventh";
 import SixQuestion from "./questions/QuestionSix";
 import ThirdQuestion from "./questions/QuestionThird";
 
-export default function Main(){
-    return(
+const steps = [1, 2, 3, 4, 5, 6, 7]
+
+
+export default function Main() {
+    return (
         <div>
-            <FirstQuestion/>
-            <SecondQuestion/>
-            <ThirdQuestion/>
-            <FourthQuestion/>
-            <FifthQuestion/>
-            <SixQuestion/>
-            <SeventhQuestion/>
+            <Stepper alternativeLabel activeStep={4} >
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel >{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            <FirstQuestion />
+            <SecondQuestion />
+            <ThirdQuestion />
+            <FourthQuestion />
+            <FifthQuestion />
+            <SixQuestion />
+            <SeventhQuestion />
+            <Button>Coninue</Button>
         </div>
     )
 }
